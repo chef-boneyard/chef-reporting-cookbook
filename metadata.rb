d87d0a6e90f954cebae3729651f739edeb6d724d
Tim Smith <tsmith84@gmail.com>
2015-09-03 08:53:43 -0700

    Add source and issues url metadata

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.3"
 
 depends "chef-client", ">= 3.0.0"
+
+source_url 'https://github.com/opscode-cookbooks/chef-reporting' if respond_to?(:source_url)
+issues_url 'https://github.com/opscode-cookbooks/chef-reporting/issues' if respond_to?(:issues_url)
