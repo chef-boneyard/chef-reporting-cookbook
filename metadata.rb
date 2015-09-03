452335c640af3ceb49b53bb6162ae0b63a82d24c
Tim Smith <tsmith@chef.io>
2015-09-03 13:23:09 -0700

    Update maintainer and make license Apache 2.0

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'chef-reporting'
-maintainer 'James Casey'
-maintainer_email 'james@opscode.com'
-license 'All rights reserved'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
 description 'Installs/Configures chef-reporting'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.3'
